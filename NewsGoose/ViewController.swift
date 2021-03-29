6571bddc2cead6e68ce6d502a779b2305f415fd2
Chad Parker <chad@chadparker.com>
2021-03-29 09:59:58 -0700

    Initial Commit

---
+++ NewsGoose/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsGoose
+//
+//  Created by Chad Parker on 3/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
