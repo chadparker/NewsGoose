9e712e9190664121169fa6e5ff2e867dc2036597
Chad Parker <chad@chadparker.com>
2021-03-29 14:22:28 -0700

    Add Core Data Stack

---
+++ NewsGoose/Model/CoreDataStack.swift
@@ -0,0 +1,46 @@
+//
+//  CoreDataStack.swift
+//  NewsGoose
+//
+//  Created by Chad Parker on 3/29/21.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "NewsGoose")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    // makes access to the context easier
+    // reminds you to use the context on the main queue
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        
+        if let error = error {
+            throw error
+        }
+    }
+}
