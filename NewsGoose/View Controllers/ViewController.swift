5bb5a7c41a849897bccb89388dd750c7614aaa28
Chad Parker <chad@chadparker.com>
2021-03-30 14:31:05 -0700

    Add search button and container view

--- NewsGoose/View Controllers/ViewController.swift
+++ NewsGoose/View Controllers/ViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        
     }
 }
