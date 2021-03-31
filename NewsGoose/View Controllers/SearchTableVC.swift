615ebc3b9dab9666a04dfdcb9d64cb489e4f7715
Chad Parker <chad@chadparker.com>
2021-03-31 15:35:41 -0700

    Add SearchTableVC and container

---
+++ NewsGoose/View Controllers/SearchTableVC.swift
@@ -0,0 +1,33 @@
+//
+//  SearchTableVC.swift
+//  NewsGoose
+//
+//  Created by Chad Parker on 3/31/21.
+//
+
+import UIKit
+
+class SearchTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
