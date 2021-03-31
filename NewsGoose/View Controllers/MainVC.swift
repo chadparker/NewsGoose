7166e3633a36f55374eeb9a3391d16c1ed7b10aa
Chad Parker <chad@chadparker.com>
2021-03-30 21:10:15 -0700

    Handle filter button presses, extract value from button text label

--- NewsGoose/View Controllers/MainVC.swift
+++ NewsGoose/View Controllers/MainVC.swift
@@ -11,12 +11,24 @@ class MainVC: UIViewController {
 
     @IBOutlet weak var searchContainer: UIView!
     var searchVC: SearchVC!
+    var postTableVC: PostTableVC!
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let searchVC = segue.destination as? SearchVC {
             searchVC.delegate = self
             self.searchVC = searchVC
+        } else if let postTableVC = segue.destination as? PostTableVC {
+            self.postTableVC = postTableVC
         }
+    }
+    
+    @IBAction func filterButtonPressed(_ sender: UIButton) {
+        let buttonText = sender.titleLabel!.text!
+        let digitsString = buttonText.replacingOccurrences(of: "\\D",
+                                                           with: "",
+                                                           options: .regularExpression,
+                                                           range: buttonText.startIndex..<buttonText.endIndex)
+        postTableVC.filter(points: Int(digitsString) ?? 0)
     }
     
     @IBAction func showSearch(_ sender: UIButton) {
