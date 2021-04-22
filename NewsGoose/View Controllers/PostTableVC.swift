db6da90fd4565f7459b6e0f3df6e574faa419169
Chad Parker <chad@chadparker.com>
2021-04-22 15:50:51 -0700

    Disable Core Data fetching in TableVCs to enable compiling project

--- NewsGoose/View Controllers/PostTableVC.swift
+++ NewsGoose/View Controllers/PostTableVC.swift
@@ -13,132 +13,81 @@ class PostTableVC: UITableViewController {
     
     var pointThreshold: Int = 0
     
-    lazy private var fetchedResultsController: NSFetchedResultsController<Post> = {
-        let moc = CoreDataStack.shared.mainContext
-        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: "dayFormatted", cacheName: nil)
-        frc.delegate = self
-        try! frc.performFetch()
-        return frc
-    }()
-    
-    private var fetchRequest: NSFetchRequest<Post> {
-        let fetchRequest: NSFetchRequest<Post> = Post.fetchRequest()
-        fetchRequest.predicate = predicate
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
-        fetchRequest.fetchLimit = 3000
-        return fetchRequest
-    }
-    
-    private var predicate: NSPredicate {
-        NSPredicate(format: "points >= %i", pointThreshold)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        tableView.register(UINib(nibName: "PostCell", bundle: nil), forCellReuseIdentifier: "PostTableCell")
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 50
-    }
+//    lazy private var fetchedResultsController: NSFetchedResultsController<Post> = {
+//        let moc = CoreDataStack.shared.mainContext
+//        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: "dayFormatted", cacheName: nil)
+//        frc.delegate = self
+//        try! frc.performFetch()
+//        return frc
+//    }()
+//    
+//    private var fetchRequest: NSFetchRequest<Post> {
+//        let fetchRequest: NSFetchRequest<Post> = Post.fetchRequest()
+//        fetchRequest.predicate = predicate
+//        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+//        fetchRequest.fetchLimit = 3000
+//        return fetchRequest
+//    }
+//    
+//    private var predicate: NSPredicate {
+//        NSPredicate(format: "points >= %i", pointThreshold)
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        tableView.register(UINib(nibName: "PostCell", bundle: nil), forCellReuseIdentifier: "PostTableCell")
+//        tableView.rowHeight = UITableView.automaticDimension
+//        tableView.estimatedRowHeight = 50
+//    }
     
     func filter(points: Int) {
         pointThreshold = points
-        performFetch()
+//        performFetch()
     }
     
-    private func performFetch() {
-        fetchedResultsController.fetchRequest.predicate = predicate
-        try! fetchedResultsController.performFetch()
-        tableView.reloadData()
-    }
-    
-    // MARK: - Table view delegate
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let post = fetchedResultsController.object(at: indexPath)
-        presentSafariVC(for: post, showing: .post)
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return fetchedResultsController.sections?.count ?? 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableCell", for: indexPath) as! PostCell
-
-        let post = fetchedResultsController.object(at: indexPath)
-        cell.post = post
-        cell.delegate = self
-
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        guard let sectionInfo = fetchedResultsController.sections?[section] else { return nil }
-        
-        return sectionInfo.name
-    }
+//    private func performFetch() {
+//        fetchedResultsController.fetchRequest.predicate = predicate
+//        try! fetchedResultsController.performFetch()
+//        tableView.reloadData()
+//    }
+//    
+//    // MARK: - Table view delegate
+//    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let post = fetchedResultsController.object(at: indexPath)
+//        presentSafariVC(for: post, showing: .post)
+//    }
+//
+//    // MARK: - Table view data source
+//
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return fetchedResultsController.sections?.count ?? 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableCell", for: indexPath) as! PostCell
+//
+//        let post = fetchedResultsController.object(at: indexPath)
+//        cell.post = post
+//        cell.delegate = self
+//
+//        return cell
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        guard let sectionInfo = fetchedResultsController.sections?[section] else { return nil }
+//        
+//        return sectionInfo.name
+//    }
 }
 
 extension PostTableVC: PostCellDelegate {
     func showComments(for post: Post) {
         presentSafariVC(for: post, showing: .comments)
-    }
-}
-
-// MARK: - FRC Delegate
-
-extension PostTableVC: NSFetchedResultsControllerDelegate {
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
-    }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
-                    didChange sectionInfo: NSFetchedResultsSectionInfo,
-                    atSectionIndex sectionIndex: Int,
-                    for type: NSFetchedResultsChangeType) {
-        switch type {
-        case .insert:
-            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
-        case .delete:
-            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
-        default:
-            break
-        }
-    }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
-                    didChange anObject: Any,
-                    at indexPath: IndexPath?,
-                    for type: NSFetchedResultsChangeType,
-                    newIndexPath: IndexPath?) {
-        switch type {
-        case .insert:
-            guard let newIndexPath = newIndexPath else { return }
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
-        case .update:
-            guard let indexPath = indexPath else { return }
-            tableView.reloadRows(at: [indexPath], with: .automatic)
-        case .move:
-            guard let oldIndexPath = indexPath,
-            let newIndexPath = newIndexPath else { return }
-            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
-        case .delete:
-            guard let indexPath = indexPath else { return }
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-        @unknown default:
-            break
-        }
     }
 }
