87b2952cc7b19d4cec545dc9fcf1022cb3d07a8b
Chad Parker <chad@chadparker.com>
2021-04-22 17:21:55 -0700

    Create PostController.fetchRecentPosts()

--- NewsGoose/Model/PostController.swift
+++ NewsGoose/Model/PostController.swift
@@ -6,13 +6,32 @@
 //
 
 import Foundation
-import CoreData
+import GRDB
 
 class PostController {
+    
+    let backgroundQueue = DispatchQueue(label: "DatabaseBackgroundQueue", qos: .userInitiated)
     
     init() {
         
     }
     
-    
+    func fetchRecentPosts(pointsThreshold: Int, completion: @escaping ([Post]) -> Void) {
+        backgroundQueue.async {
+            do {
+                try dbQueue.read { db in
+                    let posts = try Post
+                        .order(Column("date").desc)
+                        .filter(Column("points") >= pointsThreshold)
+                        .limit(3000)
+                        .fetchAll(db)
+                    DispatchQueue.main.async {
+                        completion(posts)
+                    }
+                }
+            } catch {
+                fatalError("read error")
+            }
+        }
+    }
 }
