545a53943a3c47c4317d067137cb918acc461fe4
Chad Parker <chad@chadparker.com>
2021-04-29 16:39:12 -0700

    Add importer MainView

--- NewsGoose Importer/NewsGoose Importer/MainView.swift
+++ NewsGoose Importer/NewsGoose Importer/MainView.swift
@@ -6,17 +6,37 @@
 //
 
 import SwiftUI
+import NGCore
 
 struct MainView: View {
+
+    @EnvironmentObject var importer: PostImporter
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Text("NewsGoose Importer")
+                .font(.title)
+                .padding()
+            VStack {
+                ProgressView("Importing posts…", value: Float(importer.fileCountProgress), total: Float(importer.fileCountTotal))
+                HStack {
+                    Text("\(importer.postsImportedCount) posts imported")
+                    Spacer()
+                    Text("file \(importer.fileCountProgress) of \(importer.fileCountTotal)")
+                }
+                .font(.callout.monospacedDigit())
+                .foregroundColor(.gray)
+            }
+            .frame(width: 300)
+        }
+        .padding()
+        .padding(.bottom) // window size bug
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        MainView().environmentObject(PostImporter())
     }
 }
