05637d949a4832a6a0dab7c39bfc26daafe4084e
Chad Parker <chad@chadparker.com>
2021-04-29 16:20:55 -0700

    Importer: start import in AppDelegate, pass importer to MainView

--- NewsGoose Importer/NewsGoose Importer/AppDelegate.swift
+++ NewsGoose Importer/NewsGoose Importer/AppDelegate.swift
@@ -7,18 +7,22 @@
 
 import Cocoa
 import SwiftUI
+import NGCore
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
 
-        // Create the window and set the content view.
+        try! DatabaseManager.setup()
+
+        let importer = PostImporter()
+        importer.importFromJS()
+
+        let contentView = ContentView().environmentObject(importer)
+
         window = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
             styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
