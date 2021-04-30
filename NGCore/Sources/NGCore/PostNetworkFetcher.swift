37a339f5335ef7ab3b619c37feb97bb41ddea4c8
Chad Parker <chad@chadparker.com>
2021-04-30 14:01:29 -0700

    Rename NetworkError to PostNetworkError and make public

--- NGCore/Sources/NGCore/PostNetworkFetcher.swift
+++ NGCore/Sources/NGCore/PostNetworkFetcher.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum NetworkError : Error {
+public enum PostNetworkError : Error {
     case dataTask(Error)
     case noData
     case parseError(Error)
@@ -17,7 +17,7 @@ public class PostNetworkFetcher {
 
     let baseURL = URL(string: "https://hckrnews.com/data/")!
 
-    func fetchLatest(completion: @escaping (Result<[Post], NetworkError>) -> Void) {
+    func fetchLatest(completion: @escaping (Result<[Post], PostNetworkError>) -> Void) {
         let url = baseURL.appendingPathComponent("latest.js")
 
         URLSession.shared.dataTask(with: url) { data, _, error in
@@ -42,7 +42,7 @@ public class PostNetworkFetcher {
         }.resume()
     }
 
-    func fetchDay(_ day: String, completion: @escaping (Result<[Post], NetworkError>) -> Void) {
+    func fetchDay(_ day: String, completion: @escaping (Result<[Post], PostNetworkError>) -> Void) {
         let url = baseURL.appendingPathComponent("\(day).js")
 
         URLSession.shared.dataTask(with: url) { data, _, error in
